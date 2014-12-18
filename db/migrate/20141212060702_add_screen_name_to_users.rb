afade4d763009fa9f483cc7e4d65f1bf654b2f4e
hyone <hyone.development@gmail.com>
2014-12-18 19:04:57 +0900

    Fix: careless miss on a migration file

--- db/migrate/20141212060702_add_screen_name_to_users.rb
+++ db/migrate/20141212060702_add_screen_name_to_users.rb
@@ -1,6 +1,6 @@
 class AddScreenNameToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :screen_name, :string
-    add_index :users, :screen_name, unique: true, null: false
+    add_column :users, :screen_name, :string, null: false
+    add_index :users, :screen_name, unique: true
   end
 end
