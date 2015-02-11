4fbd734b9561b9d5941dee38501701cccf48adb2
hyone <hyone.development@gmail.com>
2015-02-11 15:53:48 +0900

    Fix: revert default css syntax to SCSS

--- config/application.rb
+++ config/application.rb
@@ -23,9 +23,6 @@ module TwitterApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    # css engine
-    config.sass.preferred_syntax = :less
-
     # require by i18n-js
     config.assets.initialize_on_precompile = true
   end
