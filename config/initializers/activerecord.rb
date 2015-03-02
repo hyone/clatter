71be642405b9c3be15df930d5ea3be8157b814e6
hyone <hyone.development@gmail.com>
2015-03-02 21:48:32 +0900

    Add ActiveRecord::Base.execute

--- config/initializers/activerecord.rb
+++ config/initializers/activerecord.rb
@@ -2,3 +2,8 @@
 ActiveRecord::Base.send(:define_singleton_method , 'union', ->(*queries) {
   from "(#{ queries.map { |q| q.ast.to_sql }.join(' UNION ') }) AS #{self.table_name}"
 })
+
+
+ActiveRecord::Base.send(:define_singleton_method, 'execute', ->(query) {
+  from(query.as(table_name))
+})
